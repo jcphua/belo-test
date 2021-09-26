654152f3a0a8220ee8320637f0a4b7dd3725d6e0
JC Phua <jcphua@gmail.com>
2021-09-27 04:36:52 +1000

    Remove redundant files

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
-import './styles/App.css';
-
-import App from './App';
-
-ReactDom.render(<App rows={6} cols={6} />, document.getElementById('root'));
